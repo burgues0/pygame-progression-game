66f3ab84ec6101145b7949aaab6852a0dad4dc91
burgues0 <victordpp@outlook.com>
2023-05-28 22:18:57 -0300

    fix: main.py comment fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from settings import *
 #main game running logic
 class Game:
     def __init__(self):
+        #basic pygame setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
@@ -11,6 +12,7 @@ class Game:
     def run(self):
         while True:
             for event in pygame.event.get():
+                #checking if the game is being closed
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()    
